fc42cff5c65d7183bb810e4adcafa47c49191569
Austin Murdock <austin.d.murdock@gmail.com>
2020-08-31 00:18:58 -0700

    Fix: missing deployment ignores, SW always enabled

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 import {createSpaConfig} from '@open-wc/building-rollup';
 import typescript from '@rollup/plugin-typescript';
 
+// eslint-disable-next-line no-undef
+const developmentMode = process.env.ROLLUP_WATCH === 'true';
 const baseConfig = createSpaConfig({
   output: {
     format: 'esm',
     dir: 'dist',
   },
-  // eslint-disable-next-line no-undef
-  developmentMode: process.env.ROLLUP_WATCH === 'true',
-  injectServiceWorker: true,
+  developmentMode,
+  injectServiceWorker: developmentMode,
   nodeResolve: {
     extensions: ['.js', '.jsx', '.es6', '.es', '.mjs', '.ts', '.tsx'],
   },
