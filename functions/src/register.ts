80c28d1777ddee2b95bd16f1f77ae606338cfcfc
Austin Murdock <austin.d.murdock@gmail.com>
2020-10-03 16:30:48 -0700

    Update: name/email tracking for user maintenance

--- functions/src/register.ts
+++ functions/src/register.ts
@@ -1,10 +1,16 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-// On sign up.
-export const processSignUp = functions.auth.user().onCreate((userRecord) => {
-  const usersRef = admin.database().ref(`users/${userRecord.uid}`);
+/**
+ * Triggered during the registration process to store / set defaults for the user
+ */
+export const processSignUp = functions.auth.user().onCreate(({ displayName, email, uid }) => {
+  const usersRef = admin.database().ref(`users/${uid}`);
   return usersRef.set({
+    // Store displayName and email for account management
+    displayName,
+    email, // displayName is optional, storing email as a fallback
+    // Assign the default role for a new user
     roles: {
       user: true,
     },
