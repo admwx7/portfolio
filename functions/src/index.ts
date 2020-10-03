0d606fb85ce1b2109e06042f204ca6e7d55289f8
Austin Murdock <austin.d.murdock@gmail.com>
2020-10-03 16:31:10 -0700

    New: account deletion cleanup function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
+export * from './accountDeletion';
 export * from './character';
 export * from './register';
 export * from './roles';
